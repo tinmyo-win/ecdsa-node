994fe807608d5bd4db80d7483d9ed312071b6d02
Tin Myo Win <tinmyowin356@gmail.com>
2023-06-15 10:57:44 +0630

    removed log message for security

--- client/src/signMessage.js
+++ client/src/signMessage.js
@@ -6,18 +6,7 @@ async function signMessage(amount, privKey) {
 
     const amountHash = utf8ToBytes(amount);
     const msgHash = toHex(keccak256(amountHash));
-    // console.log(msgHash);
-    const pubKey1 = secp.getPublicKey(privKey); // Make pubkey from the private key
-    console.log(toHex(pubKey1));
     const signature = (await secp.signAsync(msgHash, privKey)).toCompactHex(); // sign
-    // const isValid = secp.verify(signature, msgHash, pubKey); // verify
-
-    // const pubKey2 = getPublicKey(secp.utils.randomPrivateKey()); // Key of user 2
-
-    // const pubKey = signature.recoverPublicKey(msgHash); // Public key recovery
-
-    // console.log(pubKey.toHex());
-    console.log(signature)
 
     return signature;
 }
